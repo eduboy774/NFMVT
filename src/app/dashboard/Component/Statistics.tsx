8cdbddb8f92746cea36fcefccdb5029e1311590d
edwin.pantaleo <edwin.pantaleo@eganet.go.tz>
2024-05-03 11:24:16 +0300

    changes on succesfull created

--- src/app/dashboard/Component/Statistics.tsx
+++ src/app/dashboard/Component/Statistics.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import CountForIncidence from "./CountForIncidence";
 
 
 export default function  Statistics(){
+
+
+  const [getIncedence, setAlIncidence] = useState([]);
+
+   // Fetch the task data from the API when the component is rendered
+   fetch("http://localhost:3000/api/get-case", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    res.json().then((data) => {
+      setAlIncidence(data); // Set the task data state with the API response
+    });
+  });
+  console.log(getIncedence);
+  
 
   return (
     <>
@@ -182,29 +199,27 @@ export default function  Statistics(){
             </div>
             <div className="overflow-x-auto">
               <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead className="text-xs text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" className="px-4 py-3">Product name</th>
-                  <th scope="col" className="px-4 py-3">Category</th>
-                  <th scope="col" className="px-4 py-3">Brand</th>
-                  <th scope="col" className="px-4 py-3">Description</th>
-                  <th scope="col" className="px-4 py-3">Price</th>
+                  <th scope="col" className="px-4 py-3">Case Number</th>
+                  <th scope="col" className="px-4 py-3">Case Description</th>
+                  <th scope="col" className="px-4 py-3">Investigator Name</th>
                   <th scope="col" className="px-4 py-3">
                     <span className="sr-only">Actions</span>
                   </th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr className="border-b dark:border-gray-700">
-                  <th scope="row"
-                      className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple iMac
-                    27&#34;</th>
-                  <td className="px-4 py-3">PC</td>
-                  <td className="px-4 py-3">Apple</td>
-                  <td className="px-4 py-3">300</td>
-                  <td className="px-4 py-3">$2999</td>
-                  <td className="px-4 py-3 flex items-center justify-end">
-                    <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown"
+                
+                  
+                    {getIncedence.map((item) => (
+          <tr className="border-b dark:border-gray-700" key={item.id}>
+            <td className="px-4 py-3">{item.id}</td>
+            <td className="px-4 py-3">{item.case_number}</td>
+            <td className="px-4 py-3">{item.case_description}</td>
+            <td className="px-4 py-3">{item.investigator}</td>
+            <td className="px-4 py-3">
+            <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown"
                             className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                             type="button">
                       <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
@@ -231,47 +246,13 @@ export default function  Statistics(){
                            className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
                       </div>
                     </div>
-                  </td>
-                </tr>
-                <tr className="border-b dark:border-gray-700">
-                  <th scope="row"
-                      className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Monitor BenQ
-                    EX2710Q
-                  </th>
-                  <td className="px-4 py-3">TV/Monitor</td>
-                  <td className="px-4 py-3">BenQ</td>
-                  <td className="px-4 py-3">354</td>
-                  <td className="px-4 py-3">$499</td>
-                  <td className="px-4 py-3 flex items-center justify-end">
-                    <button id="benq-ex2710q-dropdown-button" data-dropdown-toggle="benq-ex2710q-dropdown"
-                            className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                            type="button">
-                      <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"/>
-                      </svg>
-                    </button>
-                    <div id="benq-ex2710q-dropdown"
-                         className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                      <ul className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                          aria-labelledby="benq-ex2710q-dropdown-button">
-                        <li>
-                          <a href="#"
-                             className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
-                        </li>
-                        <li>
-                          <a href="#"
-                             className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                        </li>
-                      </ul>
-                      <div className="py-1">
-                        <a href="#"
-                           className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
+                    </td>
+
+          </tr>
+        ))}
+                 
+                    
+              
                 </tbody>
               </table>
             </div>
