0da6f6b09cbfd77fa2afdadf8021bb69bae9dd96
Joel Hillary <joelhirraly@gmail.com>
2024-05-27 15:35:48 +0300

    changes on get-all files

---
+++ src/app/api/get-all-openPorts/route.js
@@ -0,0 +1,33 @@
+import getDb from "../../database/db";
+import {GET_ALL_OPEN_PORTS} from '../../database/schema'
+
+
+export async function GET() {
+
+  const db = await getDb();
+
+  try {
+    // retrieve data from the openPorts table
+    const openPortsResponce = await db.all(GET_ALL_OPEN_PORTS);
+
+    return new Response(JSON.stringify(openPortsResponce), {
+      headers: { "content-type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return new Response({ error: "An error occurred while fetching data." }, {
+      headers: { "content-type": "application/json" },
+      status: 500,
+    });
+  } finally {
+    // Close the database connection after each request
+    if (db) {
+      // db.close()
+    }
+  }
+}
+
+
+
+
--- src/app/database/schema.js
+++ src/app/database/schema.js
@@ -24,10 +24,62 @@ export const CREATE_HTTP_HEADERS_TABLE_IF_NOT_EXIST = `
     status_code TEXT,
     content_type TEXT,
     case_uuid TEXT NOT NULL REFERENCES case_details(case_uuid) ON DELETE CASCADE ON UPDATE CASCADE
-  )`
+  )`;
+
+  export const CREATE_HTTP_TABLE_IF_NOT_EXIST = `
+  CREATE TABLE IF NOT EXISTS http (
+    http_uuid TEXT PRIMARY KEY,source_ip TEXT,
+    destination_ip TEXT,
+    method TEXT,
+    uri TEXT,
+    user_agent TEXT,
+    referer TEXT,
+    status_code TEXT,
+    content_type TEXT,
+    case_uuid TEXT NOT NULL REFERENCES case_details(case_uuid) ON DELETE CASCADE ON UPDATE CASCADE
+  )`;
 
 export const CREATE_HOSTS_TABLE_IF_NOT_EXIST = `
   CREATE TABLE IF NOT EXISTS hosts (
+  host_uuid TEXT PRIMARY KEY,
+  host_source_ip TEXT,
+  host_source_eth_mac TEXT,
+  host_source_eth_resolved TEXT,
+  host_destination_ip TEXT,
+  host_destination_eth_mac TEXT,
+  host_destination_eth_resolved TEXT,
+  case_uuid TEXT REFERENCES case_details (case_uuid) ON DELETE CASCADE ON UPDATE CASCADE
+  );
+`;
+
+export const CREATE_OPEN_PORTS_TABLE_IF_NOT_EXIST = `
+  CREATE TABLE IF NOT EXISTS openPorts (
+  host_uuid TEXT PRIMARY KEY,
+  host_source_ip TEXT,
+  host_source_eth_mac TEXT,
+  host_source_eth_resolved TEXT,
+  host_destination_ip TEXT,
+  host_destination_eth_mac TEXT,
+  host_destination_eth_resolved TEXT,
+  case_uuid TEXT REFERENCES case_details (case_uuid) ON DELETE CASCADE ON UPDATE CASCADE
+  );
+`;
+
+export const CREATE_CONNECTIOINS_TABLE_IF_NOT_EXIST = `
+  CREATE TABLE IF NOT EXISTS connections (
+  host_uuid TEXT PRIMARY KEY,
+  host_source_ip TEXT,
+  host_source_eth_mac TEXT,
+  host_source_eth_resolved TEXT,
+  host_destination_ip TEXT,
+  host_destination_eth_mac TEXT,
+  host_destination_eth_resolved TEXT,
+  case_uuid TEXT REFERENCES case_details (case_uuid) ON DELETE CASCADE ON UPDATE CASCADE
+  );
+`;
+
+export const CREATE_dnsSmbLdapServers_TABLE_IF_NOT_EXIST = `
+  CREATE TABLE IF NOT EXISTS dnsSmbLdapServers (
   host_uuid TEXT PRIMARY KEY,
   host_source_ip TEXT,
   host_source_eth_mac TEXT,
@@ -55,4 +107,10 @@ export const GET_ALL_SSDP_DATA_PAGEABLE = `SELECT * FROM ssdp LIMIT ? OFFSET ? `
 export const GET_ALL_ARP_DATA_PAGEABLE = `SELECT * FROM arp LIMIT ? OFFSET ? `;
 export const GET_ALL_SSDP_DATA = `SELECT * FROM ssdp`;
 export const GET_ALL_HTTP_HEADERS = `SELECT * FROM http_headers`;
+export const GET_ALL_HTTP = `SELECT * FROM http`;
+export const GET_ALL_OPEN_PORTS = `SELECT * FROM openPorts`;
+export const GET_ALL_CONNECTIONS = `SELECT * FROM connections`;
 export const GET_CASE_DETAILS =  `SELECT * FROM case_details`
+export const GET_dnsSmbLdapServers =  `SELECT * FROM dnsSmbLdapServers`
+
+
