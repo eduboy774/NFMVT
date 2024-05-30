9b58fc655bb06dfbb7e8ea514625631df88ef29c
alecktony <alecktony@yahoo.com>
2024-05-30 13:28:35 +0300

    new commit

---
+++ src/app/network/ssd-statistics/page.tsx
@@ -0,0 +1,119 @@
+'use client';
+import React, { useState } from 'react';
+import { HomeIcon, EyeIcon, ChartBarIcon, DocumentTextIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
+import SsdpTableDetails from './Component/SsdpTableDetails';
+import SsdpDrawing from './Component/SsdpDrawing';
+import Sidebar from '../../ui/Sidebar';
+
+const Statistics = () => {
+  const [activeItem, setActiveItem] = useState('Statistics');
+
+  const navigation = [
+    {
+      name: 'Dashboard',
+      href: '/dashboard',
+      icon: <HomeIcon className="w-5 h-5" />,
+      current: true,
+    },
+    {
+      name: 'Statistics',
+      href: '/statistics',
+      icon: <ChartBarIcon className="w-5 h-5" />,
+      current: false,
+      items: [
+        {
+          name: 'SSDP',
+          href: '/network/ssdp-requests',
+          current: false,
+          badgeCount: 5,
+        },
+        {
+          name: 'Hosts',
+          href: '/network/hosts',
+          current: false,
+          badgeCount: 12,
+        },
+        {
+          name: 'ARP',
+          href: '/network/arp-requests',
+          current: false,
+          badgeCount: 3,
+        },
+        {
+          name: 'DNS Servers',
+          href: '/network/dns-servers',
+          current: false,
+          badgeCount: 7,
+        },
+        {
+          name: 'HTTP Headers',
+          href: '/network/http-headers',
+          current: false,
+          badgeCount: 9,
+        },
+        {
+          name: 'HTTP Everything',
+          href: '/network/http-everything',
+          current: false,
+          badgeCount: 15,
+        },
+        {
+          name: 'Open Ports',
+          href: '/network/open-ports',
+          current: false,
+          badgeCount: 8,
+        },
+        {
+          name: 'Connections',
+          href: '/network/connections',
+          current: false,
+          badgeCount: 11,
+        },
+      ],
+    },
+    {
+      name: 'Visuals',
+      href: '/visuals',
+      icon: <EyeIcon className="w-5 h-5" />,
+      current: false,
+    },
+    {
+      name: 'Reports',
+      href: '/report',
+      icon: <DocumentTextIcon className="w-5 h-5" />,
+      current: false,
+    },
+    {
+      name: 'Create New Case',
+      href: '/',
+      icon: <PlusCircleIcon className="w-5 h-5" />,
+      current: false,
+    },
+  ];
+
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar navigation={navigation} activeItem={activeItem} setActiveItem={setActiveItem} />
+
+      <div className="flex-grow p-8 ml-64">
+        <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Statistics</h1>
+          </div>
+        </header>
+        <main>
+          <div className='allTableDetailsHere'>
+            <SsdpDrawing/>
+          </div>
+          <div className='allDrawingDetailsHere'>
+            <SsdpTableDetails />
+          </div>
+
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
+
