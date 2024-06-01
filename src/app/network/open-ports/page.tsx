17d1cc3885416d65bdfb92d5bad704a10e546096
Edwin Pantaleo <edwin.pantaleo@eganet.go.tz>
2024-06-01 13:33:11 +0300

    changes on correction made

--- src/app/network/open-ports/page.tsx
+++ src/app/network/open-ports/page.tsx
@@ -106,7 +106,7 @@ const Statistics = () => {
       <div className="flex-grow p-8 ml-64">
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Statistics</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Statistics For Open Ports</h1>
           </div>
         </header>
         <main>
