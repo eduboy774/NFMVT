ff7a39450aea8bac77f9f8d35130744df1e47dd3
alecktony <alecktony@yahoo.com>
2024-06-07 11:26:46 +0300

    Changes in SSDP Statistics Page

--- src/app/network/ssdp-statistics/page.tsx
+++ src/app/network/ssdp-statistics/page.tsx
@@ -92,7 +92,7 @@ const GeneralStatistics = () => {
       <div className="flex-grow p-8 ml-64">
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Statistics For Ssdp</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">SSDP Statistics</h1>
           </div>
         </header>
         <main>
